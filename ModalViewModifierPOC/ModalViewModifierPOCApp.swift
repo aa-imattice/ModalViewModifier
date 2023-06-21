d515d484de35727509d5d0791dee9caffd35515c
Ike Mattice <ike.mattice@alaskaair.com>
2023-06-21 14:43:19 -0700

    Initial Commit

---
+++ ModalViewModifierPOC/ModalViewModifierPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalViewModifierPOCApp.swift
+//  ModalViewModifierPOC
+//
+//  Created by Ike Mattice on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ModalViewModifierPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
